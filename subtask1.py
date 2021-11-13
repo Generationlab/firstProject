eb806118a42e5e832352d648383e360ddb656c42
Jason <s2010769037@fhooe.at>
2021-11-13 20:33:16 +0100

    subtask1.py

---
+++ subtask1.py
@@ -0,0 +1,11 @@
+Entry = int(input("Enter a natural number:"))
+x=1
+
+if (Entry == 0):
+    Quotient=0
+else :
+    while (Entry-x**2>=0):
+         x+=1
+Quotient = x-1
+    
+print("The largest square number for your number is:",Quotient)
---
+++ subtask2.py
@@ -0,0 +1,21 @@
+count=0
+summation=0
+minimum=0
+mean=0
+
+entry = [4,5,18,1,-1] #value of the list
+count=len(entry)
+count_upd=count-1
+print("The total number of elements in the list are",count_upd)#printing the number of elements the list without -1
+
+entry.sort() #sorting the list
+entry.pop(0) #updating the entry list with removing -1
+
+summation=sum(entry) #using the sum command to sum all elements in the list
+print("The sum of all values in the list is",summation) #printing the value of summation
+
+minimum = min(entry)
+print("The minimum value of all the elements in the list is",minimum)
+
+mean = summation/count_upd
+print("The mean value of the elements in the list is",mean)
